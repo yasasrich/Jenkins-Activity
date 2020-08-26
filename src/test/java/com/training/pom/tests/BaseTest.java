845a0eb4ccbd89fc2d58971077507b4caedc12d6
Abhiram K <4kuchibh@gmail.com>
2020-08-26 11:23:38 +0530

    Fixed more code

--- src/test/java/com/training/pom/tests/BaseTest.java
+++ src/test/java/com/training/pom/tests/BaseTest.java
@@ -2,16 +2,24 @@ package com.training.pom.tests;
 
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 
 public class BaseTest {
 	public WebDriver driver;
+	public FirefoxBinary firefoxBinary;
+	public FirefoxOptions firefoxOptions;
 	  
 	@BeforeClass
 	public void setup() {
 		// Create a new FirefoxDriver. All our test classes will use this.
-		driver = new FirefoxDriver();
+		firefoxBinary = new FirefoxBinary();
+		firefoxBinary.addCommandLineOptions("--headless");
+		firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setBinary(firefoxBinary);
+		driver = new FirefoxDriver(firefoxOptions);
 	}
 	
 	@AfterClass
